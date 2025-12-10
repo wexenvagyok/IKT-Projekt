6eff46c623285cbd89dae64f44d29a4711c30a7a
wexen <farkas.balazs.22@F4-45>
2025-12-10 12:25:00 +0100

    1. f

--- Matek/assets/js/elso.js
+++ Matek/assets/js/elso.js
@@ -16,43 +16,62 @@ function elsoFeladat() {
 
     document.querySelector('.eredmeny').innerHTML = "<div></div><div></div><div></div>";
 
-    let alpha = Number(document.getElementById("alphaSzog")?.value);
-    let beta = Number(document.getElementById("betaSzog")?.value);
-    let gamma = Number(document.getElementById("gammaSzog")?.value);
 
     let aOldal = Number(document.getElementById("aOldal")?.value);
     let bOldal = Number(document.getElementById("bOldal")?.value);
     let cOldal = Number(document.getElementById("cOldal")?.value);
 
-    if (alpha + beta + gamma > 180) {
-        showHibaUzenet("A szögek összege nem lehet nagyobb, mint 180°!");
-        return;
-    }
 
-    
     //OLDALAK
     if (aOldal != 0) {
         document.querySelectorAll('.eredmeny div')[0].innerHTML += `a: ${aOldal}`;
     } else {
-        document.querySelector('.eredmeny div')[1].innerHTML += `a: ismeretlen`;
+        document.querySelectorAll('.eredmeny div')[0].innerHTML += `a: ismeretlen`;
     }
 
     if (bOldal != 0) {
-        document.querySelector('.eredmeny div').innerHTML += `<p>b: ${bOldal}</p>`;
+        document.querySelectorAll('.eredmeny div')[1].innerHTML += `<p>b: ${bOldal}</p>`;
     } else {
-        document.querySelector('.eredmeny div').innerHTML += `<p>b: ismeretlen</p>`;
+        document.querySelectorAll('.eredmeny div')[1].innerHTML += `<p>b: ismeretlen</p>`;
     }
 
-    if (cOldal != 0) {
-        document.querySelector('.eredmeny div').innerHTML += `<p>c: ${cOldal}</p>`;
+    if (cOldal == 0) {
+        document.querySelectorAll('.eredmeny div')[2].innerHTML += `<p>c: ismeretlen</p>`;
+        
     } else {
-        document.querySelector('.eredmeny div').innerHTML += `<p>c: ismeretlen</p>`;
+        document.querySelectorAll('.eredmeny div')[2].innerHTML += `<p>c: ${cOldal}</p>`;
     }
+
     //tétel számítása
-    if (aOldal != 0 && bOldal != 0) {
-        let megoldas = aOldal**2 + bOldal**2
-        document.querySelector('.eredmeny').innerHTML += `<p>c: ${megoldas}</p>`;
-    }
+    if(aOldal !=0 && bOldal !=0 && cOldal != 0){
+        showHibaUzenet("Mindegyik adat megadásra került.");
+        return;
+    } else if (aOldal != 0 && bOldal != 0) {
+        let c = Math.sqrt(aOldal ** 2 + bOldal ** 2)
+        //let c = aOldal**2 + bOldal**2
+        let megoldas = Math.round(c * 100) / 100;
+        //let megoldas = Math.sqrt(c)
+
+        document.querySelector('.eredmeny').innerHTML += `<p>c: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+
+    } else if(bOldal != 0 && cOldal != 0){
+
+        let a = Math.sqrt(cOldal ** 2 + bOldal ** 2)
+        let megoldas = Math.round(a * 100) / 100;
+
+        document.querySelector('.eredmeny').innerHTML += `<p>a: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+
+    }  else if (aOldal != 0 && cOldal != 0) {
+
+        let b = Math.sqrt(aOldal**2 + cOldal**2)
+        let megoldas = Math.round(b * 100) / 100;
+
+        document.querySelector('.eredmeny').innerHTML += `<p>b: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+
+    } else{
+        showHibaUzenet("Legalább két adatot adj meg.");
+        return;
+    };
 
     //2 adat segítségével kiszámolja az adatokat
     // pl két szög: 180-a szög - b szög = c szög
