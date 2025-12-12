6905ba4fa40d8d9cac7405f3902d720f9c89796b
wexen <farkas.balazs.22@F4-45>
2025-12-12 11:05:39 +0100

    1. 3. jó

--- Matek/assets/js/elso.js
+++ Matek/assets/js/elso.js
@@ -24,21 +24,14 @@ function elsoFeladat() {
 
     //OLDALAK
     if (aOldal != 0) {
-        document.querySelectorAll('.eredmeny div')[0].innerHTML += `a: ${aOldal}`;
-    } else {
-        document.querySelectorAll('.eredmeny div')[0].innerHTML += `a: ismeretlen`;
+        document.querySelectorAll('.eredmeny div')[0].innerHTML += `<p>a: ${aOldal}</p>`;
     }
 
     if (bOldal != 0) {
         document.querySelectorAll('.eredmeny div')[1].innerHTML += `<p>b: ${bOldal}</p>`;
-    } else {
-        document.querySelectorAll('.eredmeny div')[1].innerHTML += `<p>b: ismeretlen</p>`;
     }
 
-    if (cOldal == 0) {
-        document.querySelectorAll('.eredmeny div')[2].innerHTML += `<p>c: ismeretlen</p>`;
-        
-    } else {
+    if (cOldal != 0) {
         document.querySelectorAll('.eredmeny div')[2].innerHTML += `<p>c: ${cOldal}</p>`;
     }
 
@@ -52,21 +45,21 @@ function elsoFeladat() {
         let megoldas = Math.round(c * 100) / 100;
         //let megoldas = Math.sqrt(c)
 
-        document.querySelector('.eredmeny').innerHTML += `<p>c: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+        document.querySelector('.eredmeny').innerHTML += `<p>c: ${megoldas}</p>`;
 
     } else if(bOldal != 0 && cOldal != 0){
 
         let a = Math.sqrt(cOldal ** 2 + bOldal ** 2)
         let megoldas = Math.round(a * 100) / 100;
 
-        document.querySelector('.eredmeny').innerHTML += `<p>a: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+        document.querySelector('.eredmeny').innerHTML += `<p>a: ${megoldas}</p>`;
 
     }  else if (aOldal != 0 && cOldal != 0) {
 
         let b = Math.sqrt(aOldal**2 + cOldal**2)
         let megoldas = Math.round(b * 100) / 100;
 
-        document.querySelector('.eredmeny').innerHTML += `<p>b: ${megoldas} /2 tizedesjegyre kerekítve/</p>`;
+        document.querySelector('.eredmeny').innerHTML += `<p>b: ${megoldas}</p>`;
 
     } else{
         showHibaUzenet("Legalább két adatot adj meg.");
